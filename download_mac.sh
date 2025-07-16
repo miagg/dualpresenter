f72fedde487f90855c5617a5b47a79e2a59b4f41
Angelos Michalopoulos <a-mi@otenet.gr>
2025-07-16 13:18:23 +0300

    Add download script for mac

---
+++ download_mac.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# This script downloads the latest release of DualPresenter from GitHub and removes the quarantine attribute.
+clear
+echo "Downloading DualPresenter..."
+echo "This may take a few seconds, please wait..."
+cd ~/Downloads || exit
+rm -f dualpresenter*.dmg
+curl -s https://api.github.com/repos/miagg/dualpresenter/releases/latest | grep -o '"browser_download_url": "[^"]*\.dmg"' | cut -d'"' -f4 | xargs wget -N -q --show-progress
+xattr -d com.apple.quarantine ~/Downloads/dualpresenter*.dmg 2>/dev/null
+echo "DualPresenter Downloaded! ✨"
+echo "You can find it in your Downloads folder."
+echo "To install, double-click the downloaded file and follow the instructions."
+echo "You may close this window now."
+exit 0
