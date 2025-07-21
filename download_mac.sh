a7a2af00f08ce466228f644b67eb1fa44e32c84b
Angelos Michalopoulos <a-mi@otenet.gr>
2025-07-21 15:46:06 +0300

    Fix download script

--- download_mac.sh
+++ download_mac.sh
@@ -5,8 +5,9 @@ echo "Downloading DualPresenter..."
 echo "This may take a few seconds, please wait..."
 cd ~/Downloads || exit
 rm -f dualpresenter*.dmg
-curl -s https://api.github.com/repos/miagg/dualpresenter/releases/latest | grep -o '"browser_download_url": "[^"]*\.dmg"' | cut -d'"' -f4 | xargs curl -O
+curl -s https://api.github.com/repos/miagg/dualpresenter/releases/latest | grep -o '"browser_download_url": "[^"]*\.dmg"' | cut -d'"' -f4 | xargs curl -O -L
 xattr -d com.apple.quarantine ~/Downloads/dualpresenter*.dmg 2>/dev/null
+echo ""
 echo "DualPresenter Downloaded! ✨"
 echo "You can find it in your Downloads folder."
 echo "To install, double-click the downloaded file and follow the instructions."
