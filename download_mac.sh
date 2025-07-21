ae0f966816f8fa0bbf8797181de9909483bf3f3c
Angelos Michalopoulos <a-mi@otenet.gr>
2025-07-21 15:34:38 +0300

    switch from wget to curl

--- download_mac.sh
+++ download_mac.sh
@@ -5,7 +5,7 @@ echo "Downloading DualPresenter..."
 echo "This may take a few seconds, please wait..."
 cd ~/Downloads || exit
 rm -f dualpresenter*.dmg
-curl -s https://api.github.com/repos/miagg/dualpresenter/releases/latest | grep -o '"browser_download_url": "[^"]*\.dmg"' | cut -d'"' -f4 | xargs wget -N -q --show-progress
+curl -s https://api.github.com/repos/miagg/dualpresenter/releases/latest | grep -o '"browser_download_url": "[^"]*\.dmg"' | cut -d'"' -f4 | xargs curl -O
 xattr -d com.apple.quarantine ~/Downloads/dualpresenter*.dmg 2>/dev/null
 echo "DualPresenter Downloaded! ✨"
 echo "You can find it in your Downloads folder."
